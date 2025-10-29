4f0ca0a396455fbc7cb4e87e5153ba1f92de52ec
martidossi <martinadossi.hello@gmail.com>
2025-10-29 13:20:27 +0100

    updated data path

--- apps/test.py
+++ apps/test.py
@@ -4,34 +4,27 @@ __generated_with = "0.15.1"
 app = marimo.App(width="columns")
 
 
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""# `Survey results` exploration – setup""")
-    return
-
-
 @app.cell
 def _():
     import marimo as mo
     import pandas as pd
-    return mo, pd
 
+    import os
+    IN_PYODIDE = os.environ.get("PYODIDE") == "1"
 
-@app.cell
-def _(pd):
-    df = pd.read_excel('not_shared/results.xlsx', sheet_name='survey data')
+    if IN_PYODIDE:
+        from pyodide.http import open_url
+        df = pd.read_csv(open_url("not_shared/results.csv"))
+    else:
+        df = pd.read_excel("not_shared/results.xlsx", sheet_name="survey data")
+
+    #df = pd.read_excel('not_shared/results.xlsx', sheet_name='survey data')
     df['condition'] = df['condition'].replace({
         'baseline': 'No AI',
         'conbands': 'Confidence bands',
         'errorbars': 'Error bars'
     })
     return (df,)
-
-
-@app.cell
-def _(df):
-    df
-    return
 
 
 @app.cell
